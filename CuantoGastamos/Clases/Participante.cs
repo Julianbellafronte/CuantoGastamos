04be576c81b694c65581aad0950b0c3ec80112f8
julian bellafronte <julianbellafronte@live.com>
2021-07-07 14:59:40 -0300

    Agregar archivos de proyecto.

---
+++ CuantoGastamos/Clases/Participante.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CuantoGastamos.Clases
+{
+    public class Participante
+    {
+        public Participante(string nombre, double monto)
+        {
+            Nombre = nombre;
+            Monto = monto;
+        }
+
+        public Participante()
+        {
+
+        }
+
+        [Required(ErrorMessage = "Se debe ingresar un nombre")]
+        public string Nombre { get; set; }
+        [Required(ErrorMessage = "Se debe ingresar un monto")]
+        public double Monto { get; set; }
+
+    }
+}
